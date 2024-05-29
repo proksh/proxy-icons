73f33db5ab77a45d412cfc77604918ef8d162d2f
Proksh Luthra <35415752+proksh@users.noreply.github.com>
2024-05-29 16:32:29 +0530

    added mapped icons

--- packages/website/src/app/page.tsx
+++ packages/website/src/app/page.tsx
@@ -1,30 +1,54 @@
-'use client';
-import Image from 'next/image';
-
 import icons from '../../icons-json.json';
 
 export default function Home() {
-  // Object.keys(icons).map((item) => {
-  //   console.log(item);
-  // });
-
   if (!icons) {
     return null;
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h3>Proxy Icons</h3>
-      {Object.keys(icons).map((category) => {
-        const categoryIcons = (icons && icons[category]) as any;
-        return (
-          categoryIcons &&
-          Object.keys(categoryIcons).map((iconName) => {
-            return <i className={'pro-' + iconName} />;
-          })
-        );
-      })}
-      <i className="pro-arrow-turn-forward-line text-2xl" />
+    <main>
+      <div className="px-16 max-w-[1400px] mx-auto">
+        <h3 className="text-8xl font-bold text-center pt-56 pb-14">Minimal, clean icons.</h3>
+        <div className="flex gap-6 mb-14">
+          <div className="flex-grow">
+            <input
+              placeholder="Search icon"
+              className="border border-gray-200 rounded-xl text-xl py-4 pl-10 pr-4 w-full"
+            />
+          </div>
+          <button className="text-xl font-medium bg-orange-500 gap-3 px-12 rounded-xl text-white">Download</button>
+        </div>
+        <div>
+          {Object.keys(icons).map((category) => {
+            const categoryIcons = (icons && icons[category as keyof typeof icons]) as any;
+            return <CategoryIcons key={category} icons={categoryIcons} categoryName={category} />;
+          })}
+        </div>
+      </div>
     </main>
+  );
+}
+
+function CategoryIcons({ icons, categoryName }: { icons: { [key: string]: any }; categoryName: string }) {
+  return (
+    <div className="mb-12">
+      <p className="mb-12">{categoryName}</p>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="flex flex-wrap gap-4">
+          {Object.keys(icons).map((iconName) => {
+            if (iconName.endsWith('-line')) {
+              return <i key={iconName} className={'text-xl pro-' + iconName} />;
+            }
+          })}
+        </div>
+        <div className="flex flex-wrap gap-4">
+          {Object.keys(icons).map((iconName) => {
+            if (iconName.endsWith('-fill')) {
+              return <i key={iconName} className={'text-xl pro-' + iconName} />;
+            }
+          })}
+        </div>
+      </div>
+    </div>
   );
 }
